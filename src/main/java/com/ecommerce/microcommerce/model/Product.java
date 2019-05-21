f16e71f1e1cd19cd1794329992227481ba8e42da
samirbouhassounwa <samir.bouhassoun@outlook.com>
2019-05-21 10:56:43 +0200

    Ajout du projet à GIT

---
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -0,0 +1,80 @@
+package com.ecommerce.microcommerce.model;
+
+
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+//@JsonIgnoreProperties(value = {"prixAchat", "id"})
+//@JsonFilter("monFiltreDynamique")
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Length(min=3, max=20 , message = "Nom trop long ou trop court. Et oui mes messages sont plus stylés que ceux de Spring")
+    private String nom;
+    @Min(value = 1)
+    private int prix;
+
+    //information que nous ne souhaitons pas exposer
+  //  @JsonIgnore
+    private int prixAchat;
+
+    public Product() {
+    }
+
+    public Product(int id, String nom, int prix, int prixAchat) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.prixAchat = prixAchat;
+    }
+
+    public int getPrixAchat() {
+        return prixAchat;
+    }
+
+    public void setPrixAchat(int prixAchat) {
+        this.prixAchat = prixAchat;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getPrix() {
+        return prix;
+    }
+
+    public void setPrix(int prix) {
+        this.prix = prix;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prix=" + prix +
+                '}';
+    }
+}
